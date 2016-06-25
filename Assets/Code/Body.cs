a5b463af56987fc30b2d1e108eff0222511bfe42
nethz <nethz@163.com>
2016-06-25 11:01:25 +0800

    aaa

---
+++ Assets/Code/Body.cs
@@ -0,0 +1,201 @@
+﻿using UnityEngine;
+using System.Collections;
+using GDGeek;
+
+public class Body : MonoBehaviour {
+
+	public Animator _animator = null;
+
+	public FSM fsm_ = new FSM();  
+
+
+	public void post(string action){
+		fsm_.post (action);
+	}
+	private State getIdle(){
+		StateWithEventMap swie = new StateWithEventMap ();
+		swie.onStart += delegate {
+
+			this._animator.speed = 1;
+			this._animator.Play("idle");
+		};
+		swie.addAction ("magic", "magic");
+		swie.addAction ("attack", delegate(FSMEvent evt) {
+			attack_++;
+			return  "attack";
+		});
+		swie.addAction ("skill", "skill");
+		swie.addAction ("def", "def");
+		return swie;
+
+	}
+	private State getAttack(){
+		StateWithEventMap swie = TaskState.Create (delegate() {
+			TaskList tl = new TaskList();
+			TaskWait tw = new TaskWait(0.01f);
+			TaskManager.PushFront(tw, delegate() {
+				this._animator.Play("attack");
+			});
+			Task task = new Task();
+			TaskManager.PushFront(task, delegate() {
+				
+			});
+			task.isOver = delegate() {
+				var info = _animator.GetCurrentAnimatorStateInfo(0);
+		
+				if(!info.IsName("attack")){
+					return true;
+				}else{
+					return false;
+				}
+			};
+			task.shutdown = delegate() {
+				attack_-- ;	
+			};
+			tl.push(tw);
+			tl.push(task);
+			tl.push(resetTask());
+			return tl;
+		}, fsm_, delegate(FSMEvent evt) {
+			if(this.attack_ != 0){
+				return "attack";
+			}else{
+				return "idle";
+			}
+		});
+		swie.addAction ("attack", delegate(FSMEvent evt) {
+			attack_ ++;
+			_animator.speed = 2+(attack_-1)*10;
+		});
+
+		return swie;
+
+	}
+
+	private State getSkill(){
+		StateWithEventMap swie = TaskState.Create (delegate() {
+			TaskList tl = new TaskList();
+			TaskWait tw = new TaskWait(0.01f);
+			TaskManager.PushFront(tw, delegate() {
+
+				this._animator.Play("skill");
+			});
+			Task task = new Task();
+
+			task.isOver = delegate() {
+				var info = _animator.GetCurrentAnimatorStateInfo(0);
+
+				if(!info.IsName("skill")){
+					return true;
+				}else{
+					return false;
+				}
+			};
+
+			tl.push(tw);
+			tl.push(task);
+			tl.push(resetTask());
+			return tl;
+		}, fsm_, delegate(FSMEvent evt) {
+
+			return "idle";
+
+		});
+		return swie;
+
+	}
+	private State getMagic(){
+		StateWithEventMap swie = TaskState.Create (delegate() {
+			TaskList tl = new TaskList();
+			TaskWait tw = new TaskWait(0.01f);
+			TaskManager.PushFront(tw, delegate() {
+
+				this._animator.Play("magic");
+			});
+			Task task = new Task();
+		
+			task.isOver = delegate() {
+				var info = _animator.GetCurrentAnimatorStateInfo(0);
+
+				if(!info.IsName("magic")){
+					return true;
+				}else{
+					return false;
+				}
+			};
+
+			tl.push(tw);
+			tl.push(task);
+			tl.push(resetTask());
+			return tl;
+		}, fsm_, delegate(FSMEvent evt) {
+
+			return "idle";
+
+		});
+		return swie;
+	}
+
+	private State getPower(){
+		StateWithEventMap swie = new StateWithEventMap ();
+		swie.onStart += delegate {
+			//	_body.power();
+		};
+		return swie;
+	}
+
+	private State getDef(){
+		StateWithEventMap swie = TaskState.Create (delegate() {
+			TaskList tl = new TaskList();
+			TaskWait tw = new TaskWait(0.01f);
+			TaskManager.PushFront(tw, delegate() {
+				_animator.Play("def");
+			});
+			Task task = new Task();
+
+			task.isOver = delegate() {
+				var info = _animator.GetCurrentAnimatorStateInfo(0);
+				if(info.normalizedTime > info.length){
+					return true;
+				}else{
+					return false;
+				}
+			};
+
+			tl.push(tw);
+			tl.push(task);
+			tl.push(resetTask());
+			return tl;
+		}, fsm_, delegate(FSMEvent evt) {
+
+			return "idle";
+
+		});
+		return swie;
+	}
+	private int attack_ = 0;
+	private Task resetTask(){
+		TweenTask tt = new TweenTask(delegate() {
+			return TweenLocalPosition.Begin(this.gameObject, 0.01f, Vector3.zero);	
+		});
+
+		return tt;
+	}
+
+	private Vector3 position_ = Vector3.zero;
+	void Start () {
+
+
+		fsm_.addState ("idle", getIdle ());
+		fsm_.addState ("attack", getAttack ());
+		fsm_.addState ("skill", getSkill ());
+		fsm_.addState ("magic", getMagic());
+		fsm_.addState ("power", getPower());
+		fsm_.addState ("def", getDef());
+
+		fsm_.init ("idle");
+
+	}
+
+
+}
---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using GDGeek;
+
+public class GameManager : MonoBehaviour {
+	private FSM fsm_ = new FSM ();
+	public WeFighter _we = null;
+	public FoeFighter _foe = null;
+	private State getBegin(){
+		StateWithEventMap swem = TaskState.Create(delegate {
+			return new Task();
+		}, this.fsm_, "fighting");
+
+
+		return swem;
+	}
+	private State getFighting(){
+		StateWithEventMap swem = new StateWithEventMap ();
+		swem.onStart += delegate {
+			Debug.Log("asdfasdfasdfsdfsdf");	
+		};
+
+
+		return swem;
+	}
+	// Use this for initialization
+	void Start () {
+
+		fsm_.addState ("begin", getBegin());
+		fsm_.addState ("fighting", getFighting ());
+
+		fsm_.init ("begin");
+	}
+	
+
+}
